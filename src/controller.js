e3d140ae9f39f4dfad99b990b069628589c8f91c
jGalie <juli.gauna09@gmail.com>
2023-10-24 15:48:44 -0300

    Correccion: getOne

--- src/controller.js
+++ src/controller.js
@@ -7,6 +7,12 @@ class LibroController{
         res.json(result);
     }
 
+    async getOne(req, res) {
+        const libro = req.body;
+        const [result] = await pool.query(`SELECT * FROM libros WHERE id = (?)`, [libroId]);
+        res.json(result);
+    }
+
     async add(req, res){
         const libro = req.body;
         const [result] = await pool.query(`INSERT INTO libros (nombre, autor, categoria, año_publicacion, ISBN) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN]);
