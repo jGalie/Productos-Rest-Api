4d1c4c98b35f68f50731e1b80653f3772db7a271
jGalie <juli.gauna09@gmail.com>
2023-10-25 01:11:39 -0300

    Exportacion de base de datos

--- script/Libros-127_0_0_1.sql
+++ script/Libros-127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-10-2023 a las 02:16:29
+-- Tiempo de generación: 25-10-2023 a las 06:03:59
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 7.4.29
 
@@ -26,15 +26,15 @@ USE `rest-api`;
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `personas`
+-- Estructura de tabla para la tabla `libros`
 --
--- Creación: 18-10-2023 a las 22:04:25
--- Última actualización: 19-10-2023 a las 00:06:28
+-- Creación: 24-10-2023 a las 16:16:06
+-- Última actualización: 25-10-2023 a las 02:13:00
 --
 
-CREATE TABLE `personas` (
+CREATE TABLE `libros` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(20) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
   `autor` varchar(30) NOT NULL,
   `categoria` varchar(30) NOT NULL,
   `año_publicacion` date DEFAULT NULL,
@@ -42,22 +42,23 @@ CREATE TABLE `personas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `personas`
+-- Volcado de datos para la tabla `libros`
 --
 
-INSERT INTO `personas` (`id`, `nombre`, `autor`, `categoria`, `año_publicacion`, `ISBN`) VALUES
-(1, 'Una Corte de Rosas y', 'Sarah J. Maas', 'Ficción histórica', '2016-05-24', '9788408155089'),
-(2, 'Casa de Tieraa y San', 'Sarah J. Mass', 'Fantasia romántica', '2020-09-17', '9788420452883'),
-(3, 'La Reina Descalza', 'Ildefonso Falcones', 'Ficción histórica', '2013-02-20', ' 978842535052');
+INSERT INTO `libros` (`id`, `nombre`, `autor`, `categoria`, `año_publicacion`, `ISBN`) VALUES
+(1, 'La Reina Descalza', 'Ildefonso Falcones', 'Ficción histórica', '2013-02-20', '978842535052'),
+(2, 'Una Corte de Rosas y Espinas', 'Sarah J. Maas', 'Ficción histórica', '2016-05-24', '9788408155089'),
+(5, 'El Príncipe del Sol', 'Claudia Ramírez Lomelí', 'Fantacia', '2019-09-03', '9788408214847'),
+(6, 'El Gremio de los Magos', 'Trudi Canavan', 'Fantasia', '2001-09-03', '9788499083445');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `personas`
+-- Indices de la tabla `libros`
 --
-ALTER TABLE `personas`
+ALTER TABLE `libros`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -65,10 +66,10 @@ ALTER TABLE `personas`
 --
 
 --
--- AUTO_INCREMENT de la tabla `personas`
+-- AUTO_INCREMENT de la tabla `libros`
 --
-ALTER TABLE `personas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `libros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
